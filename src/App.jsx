16b2e032cedadd8510278cbd05b80beaac84b861
karan kumar dua <duakaran27@gmail.com>
2022-07-16 23:29:21 +0530

    styled entire app

--- src/App.jsx
+++ src/App.jsx
@@ -45,11 +45,13 @@ const App = ()=>{
    }
   return(
     <div className="app">
-      <h1>TIC TAC TOE</h1>
+      <h1>TIC <span className="text-green">TAC</span> TOE</h1>
       <StatusMessage winner={winner} current={current}/>
       <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares}/>
-      <button onClick={onNewGame}>Start new game</button>
+      <button className={`btn-reset ${winner ? 'active':''}`} onClick={onNewGame}>Start new game</button>
+      <h2 className={{fontWeight :'normal'}}>Current game history</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove}/>
+    <div className="bg-balls"/>
     </div>
   )
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 const Square = ( {value,onClick,isWinningSquares} ) => {
   return (
-    <button type="button" className='square' onClick={onClick} style={{fontWeight : isWinningSquares ? 'bold':'normal'}}>{value}</button>
+    <button type="button"  
+    className={` square ${isWinningSquares ? 'winning' : ''} ${value==='X' ? 'text-green':'text-orange'}`}
+    onClick={onClick} style={{fontWeight : isWinningSquares ? 'bold':'normal'}}>{value}</button>
   )
 }
 

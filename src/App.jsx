7a60a7405135adb81d4fb8906d83c3d3602289e6
karan kumar dua <duakaran27@gmail.com>
2022-07-16 18:40:24 +0530

    conditional Rendring

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React,{useState} from "react";
 import Board from "./components/Board";
 import { calculateWinner } from "./helpers";
 import History from "./components/History";
+import StatusMessage from "./components/StatusMessage";
 import "./styles/root.scss";
 const App = ()=>{
   const [history,setHistory]=useState([{board:Array(9).fill(null),isXNext:true}]);
@@ -13,7 +14,7 @@ const App = ()=>{
   
   const winner=calculateWinner(current.board);
 
-  const message=winner?`Winner is ${winner}`:`Next player is ${current.isXNext?'X':'O'}`;
+  
   
   const handleSquareClick =(position)=>{
     if(current.board[position] || winner)
@@ -40,7 +41,7 @@ const App = ()=>{
   return(
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current}/>
       <Board board={current.board} handleSquareClick={handleSquareClick}/>
       <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
